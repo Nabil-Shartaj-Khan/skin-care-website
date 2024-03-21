cb65e85704de9875f3c04be7ee94037b9d5fa702
Nabil-Shartaj-Khan <shartajnabil@gmail.com>
2024-03-21 18:55:08 +0600

    added navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import './Header.css';
-import search from '../../assets/images/search.png';
-import profile from '../../assets/images/profile.png';
-import cart from '../../assets/images/cart.png';
 import logo from '../../assets/images/logo.jpg';
 
 const Header = () => {
     const [username, setUsername] = useState('');
-    const navigate = useNavigate();
 
     useEffect(() => {
         const storedUsername = localStorage.getItem('username');
@@ -19,101 +15,76 @@ const Header = () => {
 
     return (
         <nav className="navbar navbar-expand-lg navbar-light">
-            <div className='d-flex flex-column'>
-                <div className='pre-nav d-flex justify-content-around'>
-                    <div className='text-div'>
-                        <img src={search} alt="Search" style={{ width: '30px', height: '30px' }} />
-                    </div>
-                    <div className='text-div'>
-                        <img src={logo} alt="Logo" className="logo" />
-                    </div>
-                    <div className='icon-container'>
-                        <Link to={username ? "/profile" : "/login"} className="profile-link">
-                            <img src={profile} alt="Profile" />
-                            {username ? (
-                                <span className='fs-5 about-image'>{username}</span>
-                            ) : (
-                                <span className='fs-5 about-image'>Login/Register</span>
-                            )}
-                        </Link>
-                    </div>
-                </div>
-                <div className="container-fluid pt-4">
-                    <button
-                        className="navbar-toggler"
-                        type="button"
-                        data-bs-toggle="collapse"
-                        data-bs-target="#navbarNavDropdown"
-                        aria-controls="navbarNavDropdown"
-                        aria-expanded="false"
-                        aria-label="Toggle navigation"
-                    >
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div
-                        className="collapse navbar-collapse justify-content-center"
-                        id="navbarNavDropdown"
-                    >
-                        <ul className="navbar-nav d-flex flex-col ml-auto">
-                            <li className="nav-item">
-                                <Link
-                                    className="nav-link active text-white"
-                                    aria-current="page"
-                                    to='/'
-                                >Home</Link>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link text-white" href="#SkinPackages">Skin Packages</a>
-                            </li>
-                            <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle text-white" href="#" id="ShopProductsDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Shop Products
-                                </a>
-                                <ul className="dropdown-menu" aria-labelledby="ShopProductsDropdown">
-                                    <li><a className="dropdown-item" href="#">Cleansers</a></li>
-                                    <li><a className="dropdown-item" href="#">Serums</a></li>
-                                    <li><a className="dropdown-item" href="#">Moisturisers</a></li>
-                                    <li><a className="dropdown-item" href="#">Scrubs</a></li>
-                                    <li><a className="dropdown-item" href="#">LED Face Mask</a></li>
-                                    <li><a className="dropdown-item" href="#">Intimate Products</a></li>
-                                    <li><a className="dropdown-item" href="#">Make Up Removers</a></li>
-                                    <li><a className="dropdown-item" href="#">Xfoli8 Mitt</a></li>
-                                </ul>
-                            </li>
-                            <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle text-white" href="#" id="ShopFaceKitsDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Shop Face Kits
-                                </a>
-                                <ul className="dropdown-menu" aria-labelledby="ShopFaceKitsDropdown">
-                                    <li><a className="dropdown-item" href="#">Item A</a></li>
-                                    <li><a className="dropdown-item" href="#">Item B</a></li>
-                                    <li><a className="dropdown-item" href="#">Item C</a></li>
-                                </ul>
-                            </li>
-                            <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle text-white" href="#" id="SkinTreatmentsDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Skin Treatments
-                                </a>
-                                <ul className="dropdown-menu" aria-labelledby="SkinTreatmentsDropdown">
-                                    <li><a className="dropdown-item" href="#">Treatment A</a></li>
-                                    <li><a className="dropdown-item" href="#">Treatment B</a></li>
-                                    {/* Add more treatments as needed */}
-                                </ul>
-                            </li>
-                            <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle text-white" href="#" id="LaserHairRemovalDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Laser Hair Removal
-                                </a>
-                                <ul className="dropdown-menu" aria-labelledby="LaserHairRemovalDropdown">
-                                    <li><a className="dropdown-item" href="#">Option A</a></li>
-                                    <li><a className="dropdown-item" href="#">Option B</a></li>
-                                </ul>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link text-white" href="#Contact">Contact Us</a>
-                            </li>
-                        </ul>
-                    </div>
+            <div className="container">
+                <Link className="navbar-brand me-5 pe-5" to="/">
+                    <img src={logo} alt="Logo" className="logo" />
+                </Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul className="navbar-nav ml-auto">
+
+                        <li className="nav-item">
+                            <Link className="nav-link" to='/'>Home</Link>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#SkinPackages">Packages</a>
+                        </li>
+                        <li className="nav-item dropdown">
+                            <a className="nav-link dropdown-toggle" href="#" id="ShopProductsDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Shop Products
+                            </a>
+                            <ul className="dropdown-menu" aria-labelledby="ShopProductsDropdown">
+                                <li><a className="dropdown-item" href="#">Cleansers</a></li>
+                                <li><a className="dropdown-item" href="#">Serums</a></li>
+                                <li><a className="dropdown-item" href="#">Moisturisers</a></li>
+                                <li><a className="dropdown-item" href="#">Scrubs</a></li>
+                                <li><a className="dropdown-item" href="#">LED Face Mask</a></li>
+                                <li><a className="dropdown-item" href="#">Intimate Products</a></li>
+                                <li><a className="dropdown-item" href="#">Make Up Removers</a></li>
+                                <li><a className="dropdown-item" href="#">Xfoli8 Mitt</a></li>
+                            </ul>
+                        </li>
+                        <li className="nav-item dropdown">
+                            <a className="nav-link dropdown-toggle" href="#" id="ShopFaceKitsDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Shop Face Kits
+                            </a>
+                            <ul className="dropdown-menu" aria-labelledby="ShopFaceKitsDropdown">
+                                <li><a className="dropdown-item" href="#">Item A</a></li>
+                                <li><a className="dropdown-item" href="#">Item B</a></li>
+                                <li><a className="dropdown-item" href="#">Item C</a></li>
+                            </ul>
+                        </li>
+                        <li className="nav-item dropdown">
+                            <a className="nav-link dropdown-toggle" href="#" id="SkinTreatmentsDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Skin Treatments
+                            </a>
+                            <ul className="dropdown-menu" aria-labelledby="SkinTreatmentsDropdown">
+                                <li><a className="dropdown-item" href="#">Treatment A</a></li>
+                                <li><a className="dropdown-item" href="#">Treatment B</a></li>
+                                {/* Add more treatments as needed */}
+                            </ul>
+                        </li>
+                        <li className="nav-item dropdown">
+                            <a className="nav-link dropdown-toggle" href="#" id="LaserHairRemovalDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Laser Hair Removal
+                            </a>
+                            <ul className="dropdown-menu" aria-labelledby="LaserHairRemovalDropdown">
+                                <li><a className="dropdown-item" href="#">Option A</a></li>
+                                <li><a className="dropdown-item" href="#">Option B</a></li>
+                            </ul>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to={username ? "/profile" : "/login"}>
+                                {username ? (
+                                    <span>{username}</span>
+                                ) : (
+                                    <span>Login/Register</span>
+                                )}
+                            </Link>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </nav>
